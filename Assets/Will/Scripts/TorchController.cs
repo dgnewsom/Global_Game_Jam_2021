aa782ae692d2ff4b35296cccfe19f7d155902bbf
William <rockateerbusiness@gmail.com>
2021-01-29 22:27:51 +0000

    Will: Added basic torch mechanic

---
+++ Assets/Will/Scripts/TorchController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorchController : MonoBehaviour
+{
+    public int requiredSouls;
+    public float brightness;
+
+    private int currentSouls;
+    private Light torchLight;
+    private Collider detectionCollider;
+
+    private void Awake()
+    {
+        detectionCollider = GetComponent<Collider>();
+        torchLight = GetComponentInChildren<Light>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "enemy") // can be exchanged for the layer system
+        {
+            Destroy(other.gameObject);
+        }
+    }
+
+    public void lightUp()
+    {
+        detectionCollider.enabled = true;
+        torchLight.intensity = brightness;
+    }
+
+    public void putOut()
+    {
+        detectionCollider.enabled = false;
+        torchLight.intensity = 0;
+    }
+
+}
