27423939f093716e264c211b5a7554d91780ee02
A Wong <61547919+PsychoNudalz@users.noreply.github.com>
2021-01-30 02:42:21 +0000

    Anson: added handler for effects

--- Assets/Anson/Script/PlayerLightHandlerScript.cs
+++ Assets/Anson/Script/PlayerLightHandlerScript.cs
@@ -1,13 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class PlayerLightHandlerScript : MonoBehaviour
 {
+    [Header("Range")]
+    [SerializeField] float detectionRange;
+
     [Header("Material")]
     [SerializeField] List<Material> materials;
     [Header("Light")]
     [SerializeField] Light playerLight;
-    [SerializeField] float playerLightRange;
+    [SerializeField] float intensityModifier = 0.4f;
+    [SerializeField] float intensityClamp = 3f;
+    [Header("Flame")]
+    [SerializeField] VisualEffect flameEffect;
+    [SerializeField] Transform core;
+    [SerializeField] float coreMultiplier;
+    [Header("LightRing")]
+    [SerializeField] VisualEffect ringEffect;
 
+    private void FixedUpdate()
+    {
+        UpdateRange();
+    }
+
+    public void UpdateRange(float range)
+    {
+        detectionRange = range;
+    }
+
+    public void UpdateRange()
+    {
+        playerLight.range = detectionRange*1.1f;
+        playerLight.intensity = Mathf.Clamp(detectionRange * intensityModifier,0,intensityClamp);
+        flameEffect.SetFloat("Size", 1 + (detectionRange * coreMultiplier));
+        ringEffect.SetFloat("SizeRange", detectionRange);
+        //flameEffect.SetFloat("SizeRange", detectionRange);
+    }
+
+    public float DetectionRange { get => detectionRange; set => detectionRange = value; }
 }
