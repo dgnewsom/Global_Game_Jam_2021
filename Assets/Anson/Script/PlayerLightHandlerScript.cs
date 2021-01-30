b01867038bb64f29cdf367ab79ee320dd04b2d7b
A Wong <61547919+PsychoNudalz@users.noreply.github.com>
2021-01-30 02:03:03 +0000

    Anson: Added effect ring, moving on to player model handler

---
+++ Assets/Anson/Script/PlayerLightHandlerScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLightHandlerScript : MonoBehaviour
+{
+    [Header("Material")]
+    [SerializeField] List<Material> materials;
+    [Header("Light")]
+    [SerializeField] Light playerLight;
+    [SerializeField] float playerLightRange;
+
+}
