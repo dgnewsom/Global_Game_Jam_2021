1f207bac4943c64c2de0fcd2e0387c8b8d7dbd43
William <rockateerbusiness@gmail.com>
2021-01-30 22:23:29 +0000

    Will: Main menu polish

--- Assets/Will/Scripts/MainMenuController.cs
+++ Assets/Will/Scripts/MainMenuController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class MainMenuController : MonoBehaviour
@@ -17,11 +18,53 @@ public class MainMenuController : MonoBehaviour
     public Sound music;
     public Sound SFX;
 
+    private AudioSource musicVol;
+
+    private void Awake()
+    {
+        musicVol = music.gameObject.GetComponent<AudioSource>();
+    }
     private void Start()
     {
         StartCoroutine(menuAnim());
         LeanTween.alphaCanvas(fade, 0, 1).setEase(easeType);
         SFX.Play();
+    }
+
+    public void exitMenuAnim()
+    {
+        StartCoroutine(exitMenuAnimation());
+        
+    }
+
+    public void fadeOutMusicBtn()
+    {
+        StartCoroutine(fadeOutMusic());
+    }
+
+    IEnumerator fadeOutMusic()
+    {
+        while (musicVol.volume > 0)
+        {
+            musicVol.volume -= Time.deltaTime/1.5f;
+            yield return new WaitForSeconds(Time.deltaTime);
+        }
+    }
+
+    IEnumerator exitMenuAnimation()
+    {
+        float fallTime = 3;
+        foreach (GameObject art in menuArt)
+        {
+            LeanTween.moveY(art, 25, fallTime).setEase(easeType);
+            fallTime += 0.3f;
+        }
+        LeanTween.alphaCanvas(startBtn, 0, 1).setEase(easeType);
+        yield return new WaitForSeconds(1);
+        LeanTween.alphaCanvas(quitBtn, 0, 1).setEase(easeType);
+        yield return new WaitForSeconds(0.25f);
+        LeanTween.alphaCanvas(fade, 1, 1).setEase(easeType);
+        
     }
 
     IEnumerator menuAnim()
@@ -32,13 +75,14 @@ public class MainMenuController : MonoBehaviour
             LeanTween.moveY(art, 0, fallTime).setEase(easeType);
             fallTime += fallMultiplier;
         }
+
         yield return new WaitForSeconds(4);
         music.Play();
-        yield return new WaitForSeconds(5);
+        yield return new WaitForSeconds(2.75f);
         LeanTween.alphaCanvas(fade, 1, 1).setEase(easeType);
         yield return new WaitForSeconds(1);
         LeanTween.alphaCanvas(fade, 0, 1).setEase(easeType);
-        yield return new WaitForSeconds(5);
+        yield return new WaitForSeconds(2);
         LeanTween.alphaCanvas(startBtn, 1, 1).setEase(easeType);
         yield return new WaitForSeconds(1);
         LeanTween.alphaCanvas(quitBtn, 1, 1).setEase(easeType);
