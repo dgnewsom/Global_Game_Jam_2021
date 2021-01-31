0c97d9fce0e53098a498ff3190222768fcba6156
William <rockateerbusiness@gmail.com>
2021-01-31 11:41:42 +0000

    Auto stash before merge of "Will" and "origin/main"

---
+++ Assets/FadeController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class FadeController : MonoBehaviour
+{
+    private CanvasGroup fade;
+    [SerializeField] LeanTweenType easeType;
+
+    private void Awake()
+    {
+        fade = GetComponent<CanvasGroup>();
+    }
+
+    private void Start()
+    {
+        LeanTween.alphaCanvas(fade, 0, 1).setEase(easeType);
+    }
+
+    public void fadeOut()
+    {
+        LeanTween.alphaCanvas(fade, 1, 1).setEase(easeType);
+    }
+    public void fadeIn()
+    {
+        LeanTween.alphaCanvas(fade, 0, 1).setEase(easeType);
+    }
+}
